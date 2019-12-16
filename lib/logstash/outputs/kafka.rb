cfebce813ad6dcb5b27c571541270a18c25077ac
Robert Cowart <robacj@gmail.com>
2019-12-16 19:48:52 +0100

    handle empty ssl_endpoint_identification_algorithm

--- lib/logstash/outputs/kafka.rb
+++ lib/logstash/outputs/kafka.rb
@@ -353,12 +353,14 @@ class LogStash::Outputs::Kafka < LogStash::Outputs::Base
   end
 
   def set_trustore_keystore_config(props)
-    if ssl_truststore_location.nil?
-      raise LogStash::ConfigurationError, "ssl_truststore_location must be set when SSL is enabled"
+    unless ssl_endpoint_identification_algorithm.to_s.strip.empty?
+      if ssl_truststore_location.nil?
+        raise LogStash::ConfigurationError, "ssl_truststore_location must be set when SSL is enabled"
+      end
+      props.put("ssl.truststore.type", ssl_truststore_type) unless ssl_truststore_type.nil?
+      props.put("ssl.truststore.location", ssl_truststore_location)
+      props.put("ssl.truststore.password", ssl_truststore_password.value) unless ssl_truststore_password.nil?
     end
-    props.put("ssl.truststore.type", ssl_truststore_type) unless ssl_truststore_type.nil?
-    props.put("ssl.truststore.location", ssl_truststore_location)
-    props.put("ssl.truststore.password", ssl_truststore_password.value) unless ssl_truststore_password.nil?
 
     # Client auth stuff
     props.put("ssl.keystore.type", ssl_keystore_type) unless ssl_keystore_type.nil?
