7a100ef567b7a05d5cf6629745f79439500a09bd
Karol Bucek <self@kares.org>
2020-03-18 11:50:51 +0100

    Test: need more data for partition distribution
    uniform-sticky partitioner isn't the same as default in 2.3

--- spec/integration/outputs/kafka_spec.rb
+++ spec/integration/outputs/kafka_spec.rb
@@ -11,9 +11,12 @@ describe "outputs/kafka", :integration => true do
   let(:num_events) { 10 }
 
   let(:base_config) { {'client_id' => 'kafkaoutputspec'} }
+  let(:message_content) do
+    '"GET /scripts/netcat-webserver HTTP/1.1" 200 182 "-" "Mozilla/5.0 (compatible; EasouSpider; +http://www.easou.com/search/spider.html)"'
+  end
   let(:event) do
     LogStash::Event.new({ 'message' =>
-      '183.60.215.50 - - [11/Sep/2014:22:00:00 +0000] "GET /scripts/netcat-webserver HTTP/1.1" 200 182 "-" "Mozilla/5.0 (compatible; EasouSpider; +http://www.easou.com/search/spider.html)"',
+      '183.60.215.50 - - [11/Sep/2014:22:00:00 +0000] ' + message_content,
       '@timestamp' => LogStash::Timestamp.at(0)
     })
   end
@@ -137,23 +140,29 @@ describe "outputs/kafka", :integration => true do
   end
 
   context 'when using multi partition topic' do
-    let(:num_events) { 10 }
+    let(:num_events) { 100 } # ~ more than (batch.size) 16,384 bytes
     let(:test_topic) { 'logstash_integration_topic3' }
 
     before :each do
-      config = base_config.merge("topic_id" => test_topic, "partitioner" => 'org.apache.kafka.clients.producer.RoundRobinPartitioner')
-      load_kafka_data(config)
+      config = base_config.merge("topic_id" => test_topic, "partitioner" => 'org.apache.kafka.clients.producer.UniformStickyPartitioner')
+      load_kafka_data(config) do # let's have a bit more (diverse) dataset
+        num_events.times.collect do
+          LogStash::Event.new.tap do |e|
+            e.set('message', event.get('message').sub('183.60.215.50') { "#{rand(126)+1}.#{rand(126)+1}.#{rand(126)+1}.#{rand(126)+1}" })
+          end
+        end
+      end
     end
 
-    it 'should distribute events to all partition' do
+    it 'should distribute events to all partitions' do
       consumer0_records = fetch_messages(test_topic, partition: 0)
       consumer1_records = fetch_messages(test_topic, partition: 1)
       consumer2_records = fetch_messages(test_topic, partition: 2)
 
       all_records = consumer0_records + consumer1_records + consumer2_records
-      expect(all_records.size).to eq(num_events)
+      expect(all_records.size).to eq(num_events * 2)
       all_records.each do |m|
-        expect(m.value).to eq(event.to_s)
+        expect(m.value).to include message_content
       end
 
       expect(consumer0_records.size).to be > 1
@@ -166,6 +175,7 @@ describe "outputs/kafka", :integration => true do
     kafka = LogStash::Outputs::Kafka.new(config)
     kafka.register
     kafka.multi_receive(num_events.times.collect { event })
+    kafka.multi_receive(Array(yield)) if block_given?
     kafka.close
   end
 
