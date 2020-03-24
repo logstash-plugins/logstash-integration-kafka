d24ba449c324622df110271a26048398503aee73
Joao Duarte <jsvduarte@gmail.com>
2020-03-24 16:48:01 +0000

    update to centralized travis configuration
    * update gradle to 5.6
    * add .ci/run.sh with necessary kafka setup steps

---
+++ .ci/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This is intended to be run inside the docker container as the command of the docker-compose.
+
+env
+
+set -ex
+
+export KAFKA_VERSION=2.3.0
+./kafka_test_setup.sh
+
+jruby -rbundler/setup -S rspec -fd
+jruby -rbundler/setup -S rspec -fd --tag integration
+
+./kafka_test_teardown.sh
