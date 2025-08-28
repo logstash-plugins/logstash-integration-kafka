70b9758f29bcac44eaf7a7939587c9a41ac1d03c
Mashhur <mashhur.sattorov@elastic.co>
2025-08-27 17:04:44 -0700

    Simplify error chain prints.

--- lib/logstash/outputs/kafka.rb
+++ lib/logstash/outputs/kafka.rb
@@ -391,14 +391,9 @@ class LogStash::Outputs::Kafka < LogStash::Outputs::Base
 
       org.apache.kafka.clients.producer.KafkaProducer.new(props)
     rescue => e
-      cause_error = e.respond_to?(:getCause) ? e.getCause() : nil
-      logger.error("Unable to create Kafka producer from given configuration",
-                   :kafka_error_message => e,
-                   :cause => cause_error)
-      while cause_error != nil
-        logger.error("Kafka producer error chain",
-                     :kafka_error_message => cause_error,
-                     :cause => cause_error)
+      cause_error = e
+      while cause_error
+        logger.error("Kafka producer error chain", :kafka_error_message => "#{cause_error.class}: #{cause_error.message}")
         cause_error = cause_error.respond_to?(:getCause) ? cause_error.getCause() : nil
       end
       raise e
