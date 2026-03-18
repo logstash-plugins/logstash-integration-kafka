fe653cdbdf63f6ee6f28801e90085a44b0454bbd
Álex Cámara Lara <alex.camara@elastic.co>
2026-03-18 12:04:36 +0100

    Implement sasl integration tests (#239)

--- setup_keystore_and_truststore.sh
+++ setup_keystore_and_truststore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Setup Schema Registry keystore and Kafka's schema registry client's truststore
+# Setup Schema Registry and Kafka broker keystores and client's truststore
 set -ex
 
 echo "Generating schema registry key store"
@@ -8,5 +8,12 @@ keytool -genkey -alias schema_reg -keyalg RSA -keystore tls_repository/schema_re
 echo "Exporting schema registry certificate"
 keytool -exportcert -rfc -keystore tls_repository/schema_reg.jks -storepass changeit -alias schema_reg -file tls_repository/schema_reg_certificate.pem
 
-echo "Creating client's truststore and importing schema registry's certificate"
-keytool -import -trustcacerts -file tls_repository/schema_reg_certificate.pem -keypass changeit -storepass changeit -keystore tls_repository/clienttruststore.jks -noprompt
+echo "Generating Kafka broker key store"
+keytool -genkey -alias kafka_broker -keyalg RSA -keystore tls_repository/kafka_broker.jks -keypass changeit -storepass changeit -validity 365 -keysize 2048 -dname "CN=localhost, OU=John Doe, O=Acme Inc, L=Unknown, ST=Unknown, C=IT"
+
+echo "Exporting Kafka broker certificate"
+keytool -exportcert -rfc -keystore tls_repository/kafka_broker.jks -storepass changeit -alias kafka_broker -file tls_repository/kafka_broker_certificate.pem
+
+echo "Creating client's truststore and importing certificates"
+keytool -import -trustcacerts -file tls_repository/schema_reg_certificate.pem -keypass changeit -storepass changeit -keystore tls_repository/clienttruststore.jks -alias schema_reg -noprompt
+keytool -import -trustcacerts -file tls_repository/kafka_broker_certificate.pem -keypass changeit -storepass changeit -keystore tls_repository/clienttruststore.jks -alias kafka_broker -noprompt
